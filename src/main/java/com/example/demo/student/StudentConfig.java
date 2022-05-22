ec2963b8b2a392ae076ba489426100b1cc19098f
Oliver Limo <oliverlimo10@gmail.com>
2022-05-22 11:32:26 +0300

    first commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,32 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+
+            Student mariam = new Student( 
+            "Mariam", 
+            "mariam@gmail.com", 
+            LocalDate.of(2000, Month.JANUARY, 5));
+
+            Student oliver = new Student( 
+            "Oliver", 
+            "oliver@gmail.com", 
+            LocalDate.of(2001, Month.JANUARY, 5));
+
+            repository.saveAll(List.of(mariam, oliver));
+        };
+    }
+
+}
